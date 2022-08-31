f885647e2a91eb7891aaea3d4464400befb4c7b8
JunYong <jypark3@yookgak.com>
2022-08-31 16:13:18 +0900

    chore: GA 연동

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,17 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-Y92P8TWLB7', // 설정 Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
