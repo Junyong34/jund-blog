e7c60acf28ae05bd6c02da1e6b416802e8d59f94
JunYong <jypark3@yookgak.com>
2023-01-25 14:33:14 +0900

    add: meta Tag 등록

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -9,6 +9,7 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
+      <meta name="google-site-verification" content="FTWGzdvr0FXTjLtGx7cvdYGIgk5VF-xZREMsKDBdf60" />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
