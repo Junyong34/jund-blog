8278b5035178380d30425888be30c0439d6b4499
DevPark34 <wnsdyd21@gmail.com>
2025-12-22 08:23:24 +0900

    Update index.jsx

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -9,7 +9,15 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
-      <meta name="google-site-verification" content="FTWGzdvr0FXTjLtGx7cvdYGIgk5VF-xZREMsKDBdf60" />
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y92P8TWLB7"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-Y92P8TWLB7');
+      </script>
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
