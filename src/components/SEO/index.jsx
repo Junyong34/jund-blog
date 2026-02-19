622d1b578c67b74610481ac086aadbec7262f1da
junni34 <junni34@homeplus.co.kr>
2026-02-19 09:46:58 +0900

    SEO 코드 수정

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -9,15 +9,17 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y92P8TWLB7"></script>
-      <script>
+      {/* Google tag (gtag.js) */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y92P8TWLB7"
+      />
+      <script>{`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-      
         gtag('config', 'G-Y92P8TWLB7');
-      </script>
+      `}</script>
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
